4697813d30f51fd1157ff28840b4f0795be8539b
joel-becker <joel_becker@mail.com>
2020-01-23 14:40:59 -0500

    make tests depend on internet

--- tests/testthat/test-download_data.R
+++ tests/testthat/test-download_data.R
@@ -1,29 +1,50 @@
 library(stRong)
 
-data <- download_data()
+# use local defaults for download
+file_name <- "strong_data"
+file_format <- "csv"
+cloud_platform <- "googledrive"
+
+# skip tests if no internet connection for download
+check_download <- function() {
+  if (curl::has_internet() != TRUE) {
+    skip("download not available")
+  }
+}
 
 
 test_that("strong data has appropriate number of columns", {
+  check_download()
+
+  data <- download_data(file_name, file_format, cloud_platform)
+
   expect_equal(length(colnames(data)), 12)
 })
 
 
-data_missing <- data %>%
-  filter_at(vars(c("Date",
-                   "Workout Name",
-                   "Exercise Name",
-                   "Set Order",
-                   "Weight",
-                   "Weight Unit",
-                   "Reps")),
-            all_vars(is.na(.)))
-
 test_that("strong data has no missing data at 7 vital columns", {
+  check_download()
+
+  missing_data <- download_data(file_name,
+                                file_format,
+                                cloud_platform) %>%
+    filter_at(vars(c("Date",
+                     "Workout Name",
+                     "Exercise Name",
+                     "Set Order",
+                     "Weight",
+                     "Weight Unit",
+                     "Reps")),
+              all_vars(is.na(.)))
   expect_equal(nrow(data_missing), 0)
 })
 
 
 test_that("strong data has appropriate values", {
+  check_download()
+
+  data <- download_data(file_name, file_format, cloud_platform)
+
   # workout name strong never > 20 or < 4
 
   # excercise name limits
