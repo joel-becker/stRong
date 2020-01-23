562f8762762092cb6ad54fe81f9cf9231c2c0030
joel-becker <joel_becker@mail.com>
2020-01-23 13:32:25 -0500

    cols/missing data tests

--- tests/testthat/test-download_data.R
+++ tests/testthat/test-download_data.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(stRong)
+
+data <- download_data()
+
+test_that("strong data has appropriate number of columns", {
+  expect_equal(length(colnames(data)), 10)
+})
+
+data_missing <- data %>%
+  filter(any_vars(is.na(.)))
+
+test_that("strong data has no missing data", {
+  expect_equal(nrow(data_missing), 0)
 })
