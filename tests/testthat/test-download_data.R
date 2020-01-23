977531ca7e1831898d5bbcc866b73006d8ae147a
joel-becker <joel_becker@mail.com>
2020-01-23 14:29:39 -0500

    pseudocode for appropriate data values

--- tests/testthat/test-download_data.R
+++ tests/testthat/test-download_data.R
@@ -2,9 +2,11 @@ library(stRong)
 
 data <- download_data()
 
+
 test_that("strong data has appropriate number of columns", {
   expect_equal(length(colnames(data)), 12)
 })
+
 
 data_missing <- data %>%
   filter_at(vars(c("Date",
@@ -18,4 +20,17 @@ data_missing <- data %>%
 
 test_that("strong data has no missing data at 7 vital columns", {
   expect_equal(nrow(data_missing), 0)
+})
+
+
+test_that("strong data has appropriate values", {
+  # workout name strong never > 20 or < 4
+
+  # excercise name limits
+
+  # set order < 10
+
+  # weight < 1000
+
+  # reps < 100
 })
