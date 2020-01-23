e16cf552ab2da7428278bc66fe16cf3f1bc44eee
joel-becker <joel_becker@mail.com>
2020-01-23 16:29:05 -0500

    suggest wrangle_intensity_metrics decomposition

--- R/wrangle_intensity_metrics.R
+++ R/wrangle_intensity_metrics.R
@@ -6,16 +6,9 @@
 #' @keywords wrangle
 #' @export
 #' @examples
-#' wrangle_intensity_metrics()
-wrangle_intensity_metrics <- function(
-  data = data,
-  sets_exponent = 0.6,
-  aerobic_intensity = 2,
-  compound_intensity = 1.3,
-  almost_compound_intensity = 1.1
+wrangle_intensity_metrics <- function(data = data,
+                                      sets_exponent = 0.6
 ) {
-  # compute intensity metrics (for within- and between-exercise comparisons)
-
   data <- data %>%
 
     # create arbitrary intensity measure
@@ -34,15 +27,60 @@ wrangle_intensity_metrics <- function(
     # intensity wrangling by exercise type
     dplyr::mutate(
       # normalise intensity
-      normalise_intensity = intensity / mean_intensity,
-      # weight intensity measure by exercise type
-      weight_intensity = case_when(
-        exercise %in% aerobic         ~ normalise_intensity * aerobic_intensity,
-        #exercise %in% compound        ~ normalise_intensity * compound_intensity,
-        #exercise %in% almost_compound ~ normalise_intensity * almost_compound_intensity,
-        TRUE                          ~ normalise_intensity
-      )
+      normalise_intensity = intensity / mean_intensity
     )
+
+  # TODO: upweight intensity to the degree that movement involves many muscle groups
+  # use `dplyr::mutate(weight_intensity = normalise_intensity * compound_factor)` to do so
+  # perhaps same for aerobic exercise (* aerobic_factor?
 
   return(data)
 }
+
+# wrangle intensity metrics
+# # merge with exercise compoundness info
+# # within-workout
+# # # weighted by compoundness
+# # within-workout-exercise
+# # # averaging over weight/sets
+# # within-workout-exercise-set
+
+# wrangle within exercise (over sets)
+data <- data %>%
+  group_by(Date, "Workout Name", "Exercise_Name")
+
+#' @title merge_exercise_info
+#'
+#' @description merge strong data with info about exercises
+#' @param data Data from download_data()
+#' @keywords merge
+#' @examples
+#'
+#' @noRd
+
+#' @title wrangle_intensity_within_workout()
+#'
+#' @description Compute intensity metrics (for within- and between-exercise comparisons) within workout
+#' @param data Data from merge_exercise_info()
+#' @keywords wrangle
+#' @examples
+#'
+#' @noRd
+
+#' @title wrangle_intensity_within_exercise()
+#'
+#' @description Compute intensity metrics (for within- and between-exercise comparisons) within exercise
+#' @param data Data from wrangle_intensity_within_workout()
+#' @keywords wrangle
+#' @examples
+#'
+#' @noRd
+
+#' @title wrangle_intensity_within_set()
+#'
+#' @description Compute intensity metrics (for within- and between-exercise comparisons) within set
+#' @param data Data from wrangle_intensity_within_exercise()
+#' @keywords wrangle
+#' @examples
+#'
+#' @noRd
