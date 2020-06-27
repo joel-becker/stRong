754850ef030a1d64f041910942c1933bee6449d9
joel-becker <joel_becker@mail.com>
2020-06-27 13:49:00 -0400

    copy male human

--- data-raw/create_anatograms.R
+++ data-raw/create_anatograms.R
@@ -27,13 +27,19 @@ lapply(packages, library, character.only = TRUE)
 extractCoords <- function(coords, name, transMatrix) {
     c <- strsplit(coords, " ")
     c[[1]]
-
-    c[[1]][c(grep("M", c[[1]] )+1,grep("M", c[[1]] )+2)] <- NA
-
+    c[[1]][c(grep("M", c[[1]]) + 1, grep("M", c[[1]]) + 2)] <- NA
     c[[1]] <- c[[1]][grep("[[:alpha:]]", c[[1]], invert=TRUE)]
 
-    anatCoord <- as.data.frame(lapply( c, function(u)
-        matrix(as.numeric(unlist(strsplit(u, ","))),ncol=2,byrow=TRUE) ))
+    anatCoord <- as.data.frame(
+      lapply(
+        c,
+        function(u) matrix(
+          as.numeric(unlist(strsplit(u, ","))),
+          ncol=2,
+          byrow=TRUE
+        )
+      )
+    )
     anatCoord$X2[is.na(anatCoord$X1)] <- NA
     anatCoord$X1[is.na(anatCoord$X2)] <- NA
     anatCoord$id <- name
@@ -84,3 +90,62 @@ extractCoords <- function(coords, name, transMatrix) {
 
     return(anatCoord)
 }
+
+####
+#Male Human
+####
+hsMale <- read.table('homo_sapiens.male_coords.tsv', sep='\t', stringsAsFactors=F)
+hgMale_list <- list()
+for (i in 1:nrow(hsMale)) {
+    df <- extractCoords(hsMale$V2[i], hsMale$V1[i],  hsMale$V3[i])
+
+    hgMale_list[[i]] <- extractCoords(hsMale$V2[i], hsMale$V1[i],  hsMale$V3[i])
+    hgMale_list[[i]]$id <- gsub(' ', '_', hgMale_list[[i]]$id)
+    if ( (unique(hgMale_list[[i]]$id) == 'leukocyte') & (min(hgMale_list[[i]]$x, na.rm=T)<15) ) {
+        hgMale_list[[i]]$x <- hgMale_list[[i]]$x-4.5230265
+        hgMale_list[[i]]$y <- hgMale_list[[i]]$y -11.586659
+    }
+    names(hgMale_list)[i] <-  paste0(hsMale$V1[i],'-', i)
+}
+library(DescTools)
+plot(hgMale_list[['LAYER_OUTLINE-384']]$x, hgMale_list[['LAYER_OUTLINE-384']]$y)
+testis1 <- DrawEllipse(x=59.421902+ -9.6858637, y=168.88368+ -66.040746, radius.x=1, radius.y=2.6421267,  col='red')
+hgMale_list[['testis-1']] <- data.frame(X1 = testis1$x, X2 = testis1$y, id='testis', x = testis1$x, y = testis1$y, group ='testis1', stringsAsFactors = FALSE)
+testis2 <- DrawEllipse(x=66.046127+ -9.6858637, y=168.88368 + -66.040746, radius.x=1, radius.y=2.6421267,  col='red')
+hgMale_list[['testis-2']] <- data.frame(X1 = testis2$x, X2 = testis2$y, id='testis', x = testis2$x, y = testis2$y, group ='testis2', stringsAsFactors = FALSE)
+
+prostate <- DrawEllipse(x=52.737728, y= 93.289169, radius.x=3.3939276, radius.y=2.6712799,  col='red')
+hgMale_list[['prostate-1']] <- data.frame(X1 = prostate$x, X2 = prostate$y, id='prostate', x = prostate$x, y = prostate$y, group ='prostate', stringsAsFactors = FALSE)
+
+breast1 <- DrawEllipse(x=43.5, y=47, radius.x=6, radius.y=5, col='red')
+breast1 <- data.frame(x = breast1$x, y = breast1$y)
+hgMale_list[['breast-1']] <- data.frame(X1 = breast1$x, X2 = breast1$y, id='breast', x = breast1$x, y = breast1$y, group = 'breast1', stringsAsFactors = F)
+
+breast2 <- DrawEllipse(x=62, y=47, radius.x=6, radius.y=5, col='red')
+breast2 <- data.frame(x = breast2$x, y = breast2$y)
+hgMale_list[['breast-2']] <- data.frame(X1 = breast2$x, X2 = breast2$y, id='breast', x = breast2$x, y = breast2$y, group = 'breast2', stringsAsFactors = F)
+
+plot(hgMale_list[['LAYER_OUTLINE-384']]$x, hgMale_list[['LAYER_OUTLINE-384']]$y, cex=0.3)
+putGland <- DrawEllipse(x=53, y=10, radius.x=0.8, radius.y=1.6, col='red')
+
+hgMale_list[['pituitary gland-6']] <- data.frame(X1 = putGland$x, X2 = putGland$y, id='pituitary_gland', x = putGland$x, y = putGland$y, group ='pituitary_gland', stringsAsFactors = FALSE)
+#lines(hgMale_list[['pituitary gland-6']]$X1, hgMale_list[['pituitary gland-6']]$X2)
+names(hgMale_list) <- gsub('-.*', '', names(hgMale_list))
+names(hgMale_list) <- gsub(' ', '_', names(hgMale_list) )
+
+
+
+
+allAnatomy <- read.table('allOrgans.tsv', sep='\t', stringsAsFactors=F)
+organColour <- data.frame(type = c('circulation', 'nerve', 'digestion', 'respiratory','other', 'reproductive'),
+                            colour = c('red', 'purple', 'orange', 'steelblue',  '#41ab5d', '#d95f02'), stringsAsFactors=F)
+allAnatomy$colour <- organColour[match(allAnatomy$V2, organColour$type),]$colour
+colnames(allAnatomy) <- c('organ', 'type', 'colour')
+allAnatomy[allAnatomy$type=='nerve',]$type <- 'nervous_system'
+allAnatomy$organ <- gsub(' ', '_', allAnatomy$organ)
+#allAnatomy <- allAnatomy[! allAnatomy$organ %in% c('amygdala','pituitary gland'),]
+#allAnatomy <- allAnatomy[!allAnatomy$type %in% 'other',]
+hgMale_key <- allAnatomy
+hgMale_key$value <- runif(nrow(hgMale_key), 0, 20)
+
+#gganatogram(data=allAnatomy, fillOutline='#a6bddb', organism='human', sex='male', fill="colour") +facet_wrap(~type, ncol=3) +theme_classic()
